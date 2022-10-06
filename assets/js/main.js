b4d3064167c61f4fa1c7349900ba11ab491b2001
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-06 16:29:37 +0200

    finished exercice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,10 @@
-// Istruzioni
-// Il programma dovrà chiedere all'utente:
-// il numero di chilometri che vuole percorrere X
-// l'età del passeggero. X
-// calcolare il prezzo totale del viaggio
-
-/************************************************************************************/
-
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-
 // numero di chilometri che il passeggero vuole percorrere
 const userKm = Number(prompt("Quanti chilometri vuoi percorrere?"));
-console.log(userKm);
+//console.log(userKm);
 
 // età del passeggero
 const userAge = Number(prompt("Quanti anni hai?"));
-console.log(userAge);
+//console.log(userAge);
 
 // definisco il prezzo del biglietto al km
 const priceKm = 0.21;
@@ -24,13 +12,18 @@ const priceKm = 0.21;
 // prezzo totale del biglietto
 let ticketPrice = priceKm * userKm;
 
-// calcolo il valore del biglietto per gli over 65
+// calcolo il valore del biglietto per gli over 65 e i minorenni
 if (userAge >= 65) {
     let discount = ((ticketPrice * 40) / 100);
     ticketPrice = ticketPrice - discount;
-    console.log(`Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`);
+    //console.log(`Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`);
 } else if (userAge < 18) {
     let discount = ((ticketPrice * 20) / 100);
     ticketPrice = ticketPrice - discount;
-    console.log(`Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`);
+    //console.log(`Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`);
+} else {
+    ticketPrice = ticketPrice;
+    //console.log(`Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`);
 }
+
+document.getElementById('km').innerHTML = `Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`
