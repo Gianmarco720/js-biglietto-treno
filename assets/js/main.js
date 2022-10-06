56185fa8cb97d958d9ff4b3ba814148b2b6ceccb
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-06 15:04:20 +0200

    add user km and age

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Istruzioni
+// Il programma dovrà chiedere all'utente:
+// il numero di chilometri che vuole percorrere
+// l'età del passeggero.
+// calcolare il prezzo totale del viaggio
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+// numero di chilometri che il passeggero vuole percorrere
+const km = Number(prompt("Quanti chilometri vuoi percorrere?"));
+console.log(km);
+
+// età del passeggero
+const userAge = Number(prompt("Quanti anni hai?"));
+console.log(userAge);
