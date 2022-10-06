b06c223cd5b19f3b46938c3210bf4c9ad822f3c9
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-06 16:15:12 +0200

    add discount tickets

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,16 @@ console.log(userAge);
 // definisco il prezzo del biglietto al km
 const priceKm = 0.21;
 
-// calcolo il valore del biglietto
-if (userKm > priceKm) {
-    console.log(userKm * priceKm);
+// prezzo totale del biglietto
+let ticketPrice = priceKm * userKm;
+
+// calcolo il valore del biglietto per gli over 65
+if (userAge >= 65) {
+    let discount = ((ticketPrice * 40) / 100);
+    ticketPrice = ticketPrice - discount;
+    console.log(`Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`);
+} else if (userAge < 18) {
+    let discount = ((ticketPrice * 20) / 100);
+    ticketPrice = ticketPrice - discount;
+    console.log(`Il prezzo totale del biglietto è: ${ticketPrice.toFixed(2)} €`);
 }
